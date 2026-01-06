919562dcde5ae050bd259c217e543f78b6b5ffb3
Yesim Kosebasi <yesiimkoseba57@gmail.com>
2026-01-06 18:03:46 +0300

    Add ft_isalpha function

---
+++ ft_isalpha.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+#include <stdio.h>
+
+int ft_isalpha(int c)
+{
+        if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
+            return 1;
+        else
+            return 0;
+}
+
+int main(void)
+{
+    char c = 'A';
+    printf("%d\n", ft_isalpha(c)); 
+    return 0;
+}
---
+++ libft.h
@@ -0,0 +1,7 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int ft_isalpha(int c);
+
+#endif
+
