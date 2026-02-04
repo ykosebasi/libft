bdc8ff663f04f26216b06a2161da5ecf81a1dabc
Yesim Kosebasi <yesiimkoseba57@gmail.com>
2026-02-04 03:56:37 +0300

    part 2 mainlerindeyim

--- main.c
+++ main.c
@@ -3,109 +3,7 @@
 
 
 
-// int main(void)
-// {
-//     char dest[20] = "hello ";
-//     char src[] = "world";
-//     ft_strlcat(dest, src, 11);
 
-//     printf("%s\n", dest);
-//     return (0);
-// }
-
-// int main(void)
-// {
-// 	char dest[] = "";
-// 	char src[] = "qwrher";
-// 	int size = 4;
-// 	ft_strlcpy(dest, src, size);
-// 	printf("%s\n", dest);
-// }
-
-// int main(void)
-// {
-//     char *src[] = "hello";
-//     printf("%ld\n",ft_strlen(src));
-//     return(0);
-// }
-
-
-// int main(void)
-// {
-//     char *src = "Merhaba İstanbul";
-//     char *dest = "tan";
-//     printf("%s\n", ft_strnstr(src, dest, 20));
-// }
-
-// int	main(void)
-// {
-// 	char *s = "Merhaba";
-// 	printf("%s\n", ft_strchr(s, 'y'));
-// 	return (0);
-// }
-
-// int	main(void)
-// {
-// 	char *s = "Merhaba";
-// 	printf("%s\n", ft_strrchr(s, 'a'));
-// 	return (0);
-// }
-
-// int main(void)
-// {
-//     printf("%d\n", ft_strncmp("merhaba", "metin", 2));
-// }
-
-// int main(void)
-// {
-//     char *str = "+1234ab";
-//     printf("%d\n", ft_atoi(str));
-// }
-
-// int main (void)
-// {
-// 	char s[6] = "hello";
-// 	printf("%s\n", s);
-// 	ft_bzero(s, 6);
-// 	printf("%s\n", s);
-// }
-
-// int main(void)
-// {
-// 	char dest[] = "hello";
-// 	char src[] = "dunya";
-// 	ft_memcpy(dest, src, 0);
-// 	printf("%s\n", dest);
-// }
-
-// int main()
-// {
-// 	char str[] = "abcd";
-// 	ft_memmove(str + 1, str, 4);
-// 	printf("%s\n", str );
-// }
-
-// int main()
-// {
-// 	char str[] = "merhaba";
-// 	char *p = ft_memchr(str, 'h', 7);
-// 	printf("%s\n", p);
-// }
-
-// int main()
-// {
-// 	char str1[] = "mea";
-// 	char str2[] = "mer";
-// 	printf("%d\n",ft_memcmp(str1, str2, 3));
-// }
-
-// int main()
-// {
-// 	char str[] = "merhaba";
-// 	char *p;
-// 	p = ft_memset(str, 'y', 2);
-// 	printf("%s\n", p);
-// }
 
 // int main()
 // {
@@ -120,6 +18,7 @@
 // 	printf("copy : %s\n", copy);
 // 	free(copy);
 // }
+
 
 // int main()
 // {
@@ -526,7 +425,7 @@
 // 	printf("%s\n", s);
 // }
 
-								// BU MEMSET İN İNT Lİ HALİ;
+						//******ÖĞREN		// BU MEMSET İN İNT Lİ HALİ;
 // int main()
 // {
 // 	int s[] = {1, 2, 3, 4, 5};
@@ -535,3 +434,66 @@
 // }
 
 
+// int main()
+// {
+// 	char d1[20] = "hello ";
+// 	char s1[6] = "world";
+// 	ft_memcpy(d1, s1, 6);
+// 	printf("%s\n", d1);
+// }
+
+
+// int main()
+// {
+// 	char str[] = "ABCDE123";
+// 	ft_memmove(str + 4 , str, 4);
+// 	printf("%s\n", str);
+// }
+
+
+
+// int main()
+// {
+// 	char str[] = "hello";
+// 	char *p = ft_memchr(str, 'l', 5);
+// 	printf("%s\n", p);
+// }
+
+
+			// ARADAKİ FARKI ANLAMAK ADINA STRNCMP Yİ DE EKLEDİM O NULL GÖRÜNCE STRİNG BİTTİ DİYO VE KARŞILAŞTIRMAYI BİTİRİYO 0 DÖNDÜRÜYOR
+			// AMA MEMCMP NULL A BAKMAZ VE L - P FARKINI DÖNDÜRÜR
+// int main()
+// {
+// 	char str1[] = "hel\0lo";
+// 	char str2[] = "hel\0p";
+// 	printf("%d\n", ft_memcmp(str1, str2, 6));
+// 	printf("%d\n", ft_strncmp(str1, str2, 6));
+// }
+
+
+
+// int main()
+// {
+// 	char str[] = " 		-58630";
+// 	printf("%d\n",ft_atoi(str));
+// }
+
+
+ 				//BUNUN MAİNİNİ SOR!!!
+// int main()
+// {
+// 	char *copy = ft_calloc(8, 1);
+// 	printf("%s\n", copy);
+// }
+
+
+
+// int main()
+// {
+// 	char *now = "hello";
+// 	char *ptr;
+
+// 	ptr = ft_strdup(now);
+// 	printf("%s\n", ptr);
+// 	free(ptr);
+// }
